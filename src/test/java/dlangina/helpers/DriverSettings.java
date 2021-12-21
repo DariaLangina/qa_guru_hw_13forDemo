9348a3b02e8d44bc0f3f1676653723f2886499c0
DariaLangina <dlangina@rvision.ru>
2021-12-21 07:56:17 +0300

    added Configuration.timeout

--- src/test/java/dlangina/helpers/DriverSettings.java
+++ src/test/java/dlangina/helpers/DriverSettings.java
@@ -14,6 +14,7 @@ public class DriverSettings {
     Configuration.browserVersion = Project.config.browserVersion();
     Configuration.browserSize = Project.config.browserSize();
 //        Configuration.baseUrl = App.config.webUrl();
+    Configuration.timeout = 10000;
 
     DesiredCapabilities capabilities = new DesiredCapabilities();
     ChromeOptions chromeOptions = new ChromeOptions();
